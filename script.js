d8eb5a88f2faf3dde4645fc0f92ee3a7354d1c76
bioroid17 <trace1997@ajou.ac.kr>
2023-08-20 04:30:08 +0900

    MEME MAKER - Adding Text

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
@@ -16,6 +17,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value; // 5
+ctx.lineCap = "round"; // 5
 let isPainting = false;
 let isFilling = false;
 
@@ -83,7 +85,18 @@ function onFileChange(event) {
 		fileInput.value = null;
 	};
 }
+function onDoubleClick(event) {
+	const text = textInput.value;
+	if (text !== "") {
+		ctx.save();
+		ctx.lineWidth = 1;
+		ctx.font = "48px serif";
+		ctx.fillText(text, event.offsetX, event.offsetY);
+		ctx.restore();
+	}
+}
 
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
